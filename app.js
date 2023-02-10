558ad3ba0816edf6fb26da754a86a6d9d31d3508
Federico Cáceres <federicoenmanuel@gmail.com>
2023-02-10 20:16:56 -0300

    update

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send(`<h1>Hola ${req.query.nombre}!</h1>`);
+  let name = req.query.nombre ? req.query.nombre : 'desconocido';
+  res.send(`<h1>Hola ${name}!</h1>`);
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
